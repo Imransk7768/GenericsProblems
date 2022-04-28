597681ee6bfb2ac965057672de4016103e6658fe
Imransk7768 <imransk7768@gmail.com>
2022-04-29 01:34:55 +0530

    [Imran] Added: Printing All DataTypes Array Values

--- GenericsProblems/Program.cs
+++ GenericsProblems/Program.cs
@@ -8,7 +8,17 @@ namespace GenericsProblems
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(">>>>  Welcome to GENERICS  <<<<");
+            Console.WriteLine(">>>>  Welcome to GENERICS  <<<<\n");
+            int[] intArr = { 1, 2, 3, 4, 5, 6 };
+            double[] doubleArr = { 1.2, 2.3, 3.4, 4.5, 5.6 };
+            char[] charArr = { 'a', 'b', 'c', 'd', 'e', 'f' };
+            PrintArray array = new PrintArray();
+            Console.WriteLine("Values of Integer Array : ");
+            array.ToPrint(intArr);
+            Console.WriteLine("Values of double Array :");
+            array.ToPrint(doubleArr);
+            Console.WriteLine("Values of Char Array :");
+            array.ToPrint(charArr);
         }
     }
 }
