3b3bca008658f91483299386b44400ad7676c603
Imransk7768 <imransk7768@gmail.com>
2022-04-29 01:28:02 +0530

    [Imran] Added: Displays Welcome Message

---
+++ GenericsProblems/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace GenericsProblems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(">>>>  Welcome to GENERICS  <<<<");
+        }
+    }
+}
